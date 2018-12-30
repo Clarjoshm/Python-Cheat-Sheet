4c24cd2699240d33ec821102fc5a85a94fb326d1
Josh <clarjoshm11@gmail.com>
2018-12-30 15:59:11 +0000

    Syntax

--- Codecademy.py
+++ Codecademy.py
@@ -1,3 +1,7 @@
 # Using a PRINT statement,
 # output a message of your choosing to the terminal.
 print("Im learning python")
+
+# Print something using Python 3's SYNTAX.
+print("I'm learning python")
+print("Its going well.....")
