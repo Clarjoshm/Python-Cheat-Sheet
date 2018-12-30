5d0bdec1ea15dc8b81fadfd834e39df3f4a81aed
Josh <clarjoshm11@gmail.com>
2018-12-30 16:00:08 +0000

    Print Statement

--- Codecademy.py
+++ Codecademy.py
@@ -5,3 +5,7 @@ print("Im learning python")
 # Print something using Python 3's SYNTAX.
 print("I'm learning python")
 print("Its going well.....")
+
+# Try adding your name to the PRINT STATEMENT with
+# the + operator so that this Python program prints "Hello [your_name]"
+print("Hello "+"Josh, ")
